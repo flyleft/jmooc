dcd6a68a11ff25c3453014cde1279e114f8dd2c2
jcala <jcalaz@163.com>
2016-12-27 20:28:39 +0800

    完成了对首页的改造

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,302 +1,214 @@
-/*   
-Template Name: eLearn Multi-Purpose HTML Template - Learning made easy
-Version: 1.0
-Author: KL-Webmedia
-*/
-
-$(document).ready(function () {
-    
-    // GLOBAL - optimization
+$(document).ready(function() {
     var $window = $(window);
-    
-    /* =========================
-    Header show screen (Home 1)
-    ========================= */
-    var $screens = $('.screens'), height_prev=0, height_max;
-    $screens.find('img').each(function (idx) {
-        var $this = $(this), height = $this.height(), width = $this.width();
-        height_max = height_prev > height ? height_prev : height; // search heighter img
-        switch(idx) {
-            case 0: $this.css('left', 0); break;
-            case 1: $this.css('right', 0); break;
-            case 2: $this.css({'left':'50%', 'margin-left':-(width/2)}); break;
+    var $screens = $('.screens'),
+        height_prev = 0,
+        height_max;
+    $screens.find('img').each(function(idx) {
+        var $this = $(this),
+            height = $this.height(),
+            width = $this.width();
+        height_max = height_prev > height ? height_prev : height;
+        switch (idx) {
+            case 0:
+                $this.css('left', 0);
+                break;
+            case 1:
+                $this.css('right', 0);
+                break;
+            case 2:
+                $this.css({
+                    'left': '50%',
+                    'margin-left': -(width / 2)
+                });
+                break
         }
     });
-    $screens.find('p').height(height_max); // set heightest img to contaner P
-    
-    
-    /* ===========================================================
-    Slider (Carousel, Home 2&3) - full width slide + anim slide up
-    =========================================================== */
+    $screens.find('p').height(height_max);
     var $carusel = $('#carousel-full, #carousel-anim'),
-        animItemSlider = function () {
+        animItemSlider = function() {
             var $this = $(this);
-            animate.itemsInside($this).each(function () {
+            animate.itemsInside($this).each(function() {
                 var $this = $(this);
-                animate.start($this);
+                animate.start($this)
             });
-            $this.find('.item.active img').addClass('active');
+            $this.find('.item.active img').addClass('active')
         },
-        animHideItemSlider = function () {
+        animHideItemSlider = function() {
             var $this = $(this);
-            animate.itemsInside($this).each(function () {
+            animate.itemsInside($this).each(function() {
                 var $this = $(this);
                 animate.remove($this);
-                animate.itemsHide($carusel); 
-                //animItemHide();
+                animate.itemsHide($carusel)
             });
-            $this.find('.item img').removeClass('active');
-        }; 
-    $carusel.on('slid.bs.carousel', animItemSlider); // event: end of sliding
-    $carusel.on('slide.bs.carousel', animHideItemSlider); // event: start of sliding
-    $carusel.each(animItemSlider); // start, before slider events
-    $carusel.find('.item.active img').addClass('active'); // anim Img (Home 3)
-    
-    /* carousel - configuration, optional
-    $carusel.carousel({
-        interval: 5000, // The amount of time to delay between automatically cycling an item. If false, carousel will not automatically cycle.
-        pause: 'hover', // Pauses the cycling of the carousel on mouseenter and resumes the cycling of the carousel on mouseleave.
-        wrap: true      // Whether the carousel should cycle continuously or have hard stops.
-    });
-    */
-    
-
-    /* ===================
-    Plugin: Easy pie chart
-    ====================== */
-    var $charts = $('.chart'), 
-        $firstChart = $('.chart').first(), 
-        bPieChartWasSeeing = false, 
-        pieChart = function() { 
-            $charts.easyPieChart({
-                barColor: '#7FCF4E', // The color of the curcular bar. You can pass either a css valid color string like rgb, rgba hex or string colors. But you can also pass a function that accepts the current percentage as a value to return a dynamically generated color.
-                trackColor: '#DAE4F7', // The color of the track for the bar, false to disable rendering.
-                scaleColor: 'transparent', // The color of the scale lines, false to disable rendering.
-                lineCap: 'butt', // Defines how the ending of the bar line looks like. Possible values are: butt, round and square.
-                lineWidth: 16, // Width of the bar line in px.
-                size: 185, // Size of the pie chart in px. It will always be a square.
-                animate: 1500, // Time in milliseconds for a eased animation of the bar growing, or false to deactivate.
-                //onStart: $.noop, // Callback function that is called at the start of any animation (only if animate is not false).
-                //onStop: $.noop, // Callback function that is called at the end of any animation (only if animate is not false).
-                //easing: 'easeOutBounce',
-                onStep: function(from, to, percent) {
-                    $(this.el).find('.percent').text(Math.round(percent));
-                }
-            });
-            bPieChartWasSeeing = true; // user have seen Pie Chart
-            //pieChart = function () {}; // function pieChart() will be start only 1 times !
+            $this.find('.item img').removeClass('active')
         };
-    if($charts.length) { // if Chart is on site
-        if($window.height() + $window.scrollTop() - $firstChart.offset().top > 0) { // if Pie Chart is on screen or above - start animation ...
-            pieChart();
-        } else {
-            $window.scroll(function() { // ... else monit event Scroll
-                if(!bPieChartWasSeeing  &&  $window.height() + $window.scrollTop() - $firstChart.offset().top > 0) {
-                    pieChart();
+    $carusel.on('slid.bs.carousel', animItemSlider);
+    $carusel.on('slide.bs.carousel', animHideItemSlider);
+    $carusel.each(animItemSlider);
+    $carusel.find('.item.active img').addClass('active');
+    var $charts = $('.chart'),
+        $firstChart = $('.chart').first(),
+        bPieChartWasSeeing = false,
+        pieChart = function() {
+            $charts.easyPieChart({
+                barColor: '#7FCF4E',
+                trackColor: '#DAE4F7',
+                scaleColor: 'transparent',
+                lineCap: 'butt',
+                lineWidth: 16,
+                size: 185,
+                animate: 1500,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent))
                 }
             });
+            bPieChartWasSeeing = true
+        };
+    if ($charts.length) {
+        if ($window.height() + $window.scrollTop() - $firstChart.offset().top > 0) {
+            pieChart()
+        } else {
+            $window.scroll(function() {
+                if (!bPieChartWasSeeing && $window.height() + $window.scrollTop() - $firstChart.offset().top > 0) {
+                    pieChart()
+                }
+            })
         }
     }
-    
-    
-    /* ============================================
-    Bootstrap: Togglable tabs - Profil Create Stage
-    ============================================ */
     var $createStageToggle = $('#create-stage-toggle');
-    $createStageToggle.find('a').click(function (e) {
+    $createStageToggle.find('a').click(function(e) {
         var $this = $(this);
         e.preventDefault();
-        $createStageToggle.children().removeClass('active'); // remove last active
-        $this.parent().addClass('active'); // set new active link
-        $this.tab('show'); // show tab
+        $createStageToggle.children().removeClass('active');
+        $this.parent().addClass('active');
+        $this.tab('show')
     });
-    
-
-    /* ====================================
-    Bootstrap: Togglable tabs - Testimonial
-    ==================================== */
-    var $faces = $('.testimonial .face'),  $quotes = $('.testimonial .quote'), aFaces=[], iFacesNum, iMaxFaces=42;
-    $faces.find('a').append('<span />'); /* add color filter */
-    $faces.find('a').each(function () {
-        aFaces.push('<a href="'+this.href+'">' + $(this).html() + '</a>');
-        
+    var $faces = $('.testimonial .face'),
+        $quotes = $('.testimonial .quote'),
+        aFaces = [],
+        iFacesNum, iMaxFaces = 42;
+    $faces.find('a').append('<span />');
+    $faces.find('a').each(function() {
+        aFaces.push('<a href="' + this.href + '">' + $(this).html() + '</a>')
     });
-    // testimonial container must have 42 faces, 
-    // if we put less the rest will be filled with the same randomly chosen
     iFacesNum = aFaces.length;
-    if(iFacesNum < iMaxFaces) {
-        for(var i=iFacesNum; i<iMaxFaces; i+=1) {
-            $faces.append(aFaces[Math.floor(Math.random()*iFacesNum)]); // random face
+    if (iFacesNum < iMaxFaces) {
+        for (var i = iFacesNum; i < iMaxFaces; i += 1) {
+            $faces.append(aFaces[Math.floor(Math.random() * iFacesNum)])
         }
     }
-    $faces.find('a').click(function(e) { /* event Click and change quote */
+    $faces.find('a').click(function(e) {
         var $this = $(this);
         e.preventDefault();
-        $faces.find('a').removeClass('active'); // remove last active
-        $this.addClass('active'); // set new active link
-        $this.tab('show'); // show tab
+        $faces.find('a').removeClass('active');
+        $this.addClass('active');
+        $this.tab('show')
     });
-    
-    $faces.find('a:eq(20)').click(); // check middle face
-
-
-    /* ==========================
-    Video - set player
-    ========================== */
+    $faces.find('a:eq(20)').click();
     var $video = $('.video-prev');
-    $video.append('<i class="fa fa-play" />'); /* play sign */
-    $video.append('<span class="overlay" />'); /* overlay */
-    $('.video-play-here').click(function () { /* player load in the same place */
+    $video.append('<i class="fa fa-play" />');
+    $video.append('<span class="overlay" />');
+    $('.video-play-here').click(function() {
         $(this).html($('<iframe />', {
             src: this.href
         }));
-        return false;
+        return false
     });
-    
-    
-    /* ==========================
-    Plugin: cuteTime
-    ========================== */    
     $('time.cute-time').cuteTime();
-    
-    
-    /* ==========================
-    Bootstrap: popover (tooltip)
-    ========================== */    
     $('.our-team .btn').popover({
-        animation: true, // apply a CSS fade transition to the tooltip
-        trigger: 'hover', // how popover is triggered - click | hover | focus | manual
-        delay: {show: 500, hide: 0} // delay showing and hiding the popover (ms) - does not apply to manual trigger type ...
-        //more - http://getbootstrap.com/javascript/#popovers
+        animation: true,
+        trigger: 'hover',
+        delay: {
+            show: 500,
+            hide: 0
+        }
     });
-
-
-    /* ===========================
-    Select depend - form register
-    =========================== */
-    var $selectCountry = $('#fieldSelectCountry'), $selectCity = $('#fieldSelectCity');
-    if($selectCountry.length && $selectCity.length) { // if script find the form with select country
+    var $selectCountry = $('#fieldSelectCountry'),
+        $selectCity = $('#fieldSelectCity');
+    if ($selectCountry.length && $selectCity.length) {
         $selectCity.find('option').prop('selected', false).hide();
         $selectCity.find('option.' + $selectCountry.val()).show();
-        $selectCountry.change(function () {
+        $selectCountry.change(function() {
             $selectCity.find('option').prop('selected', false).hide();
-            $selectCity.find('option.' + this.value).show();
-        });
+            $selectCity.find('option.' + this.value).show()
+        })
     }
-
-
-    /* =============================
-    Social URL Input - form register
-    ============================= */    
-    if($('form .btn-social').length) {
+    if ($('form .btn-social').length) {
         $('form .input-social').hide();
-        $('form .btn-social').click(function () {
-            var $this = $(this), $fieldInputSocialURL;
-            if($this.hasClass('social-facebook')) {
-                $fieldInputSocialURL = $('#fieldInputFacebook');
+        $('form .btn-social').click(function() {
+            var $this = $(this),
+                $fieldInputSocialURL;
+            if ($this.hasClass('social-facebook')) {
+                $fieldInputSocialURL = $('#fieldInputFacebook')
+            } else if ($this.hasClass('social-youtube')) {
+                $fieldInputSocialURL = $('#fieldInputYoutube')
+            } else if ($this.hasClass('social-twitter')) {
+                $fieldInputSocialURL = $('#fieldInputTwitter')
+            } else if ($this.hasClass('social-gplus')) {
+                $fieldInputSocialURL = $('#fieldInputGplus')
+            } else if ($this.hasClass('social-dribbble')) {
+                $fieldInputSocialURL = $('#fieldInputDribbble')
+            } else if ($this.hasClass('social-linkedin')) {
+                $fieldInputSocialURL = $('#fieldInputLinkedin')
+            } else if ($this.hasClass('social-tumblr')) {
+                $fieldInputSocialURL = $('#fieldInputTumblr')
             }
-            else if($this.hasClass('social-youtube')) {
-                $fieldInputSocialURL = $('#fieldInputYoutube');
-            }
-            else if($this.hasClass('social-twitter')) {
-                $fieldInputSocialURL = $('#fieldInputTwitter');
-            }
-            else if($this.hasClass('social-gplus')) {
-                $fieldInputSocialURL = $('#fieldInputGplus');
-            }
-            else if($this.hasClass('social-dribbble')) {
-                $fieldInputSocialURL = $('#fieldInputDribbble');
-            }
-            else if($this.hasClass('social-linkedin')) {
-                $fieldInputSocialURL = $('#fieldInputLinkedin');
-            }
-            else if($this.hasClass('social-tumblr')) {
-                $fieldInputSocialURL = $('#fieldInputTumblr');
-            }
-
-            if($this.hasClass('btn-gray')) {
+            if ($this.hasClass('btn-gray')) {
                 $this.removeClass('btn-gray');
-                $fieldInputSocialURL.fadeIn().focus();
-            }
-            else {
+                $fieldInputSocialURL.fadeIn().focus()
+            } else {
                 $this.addClass('btn-gray');
-                $fieldInputSocialURL.val('').fadeOut();
+                $fieldInputSocialURL.val('').fadeOut()
             }
-            return false;
-        });
+            return false
+        })
     }
-
-    
-    /* =============================
-    Curtain - form application 
-    ============================= */ 
-    $('form .form-curtain').click(function () {
+    $('form .form-curtain').click(function() {
         $(this).fadeOut();
-        return false;
+        return false
     });
-
-    
-    /* =============================
-    Submit link - form search blog 
-    ============================= */ 
-    $('form.search-blog .submit').click(function () {
-        var $form = $(this).parent(), $inputText = $form.find('input[type="search"]');
-        if($inputText.val() != '') {
+    $('form.search-blog .submit').click(function() {
+        var $form = $(this).parent(),
+            $inputText = $form.find('input[type="search"]');
+        if ($inputText.val() != '') {
             $form.submit();
-            $form.find('i.fa').attr('class', 'fa fa-spinner fa-spin'); /* loader */
+            $form.find('i.fa').attr('class', 'fa fa-spinner fa-spin')
         }
-        return false;
+        return false
     });
-
-    
-    /* =============================
-    Category boxes - button More
-    ============================= */
-    $('#loadCategoryMore').click(function () {
+    $('#loadCategoryMore').click(function() {
         var $button = $(this);
-        $('.category-box-row-hidden').slideDown('slow', function () {
-            $button.fadeOut();
+        $('.category-box-row-hidden').slideDown('slow', function() {
+            $button.fadeOut()
         });
-        return false;
+        return false
     });
-
-
-    /* =============================
-    Plugin: prettyPhoto (zoom image)
-    ============================= */
     var $prettyPhoto = $('a[rel^="prettyPhoto"]');
-    if($prettyPhoto.length) {
+    if ($prettyPhoto.length) {
         $prettyPhoto.prettyPhoto({
-            animation_speed: 'fast', /* fast/slow/normal */
-            slideshow: 5000, /* false OR interval time in ms */
-            autoplay_slideshow: false, /* true/false */
-            opacity: 0.80, /* Value between 0 and 1 */
-            show_title: true, /* true/false */
-            allow_resize: true, /* Resize the photos bigger than viewport. true/false */
+            animation_speed: 'fast',
+            slideshow: 5000,
+            autoplay_slideshow: false,
+            opacity: 0.80,
+            show_title: true,
+            allow_resize: true,
             default_width: 500,
             default_height: 344,
-            counter_separator_label: '/', /* The separator for the gallery counter 1 "of" 2 */
-            theme: 'pp_default', /* light_rounded / dark_rounded / light_square / dark_square / facebook */
-            horizontal_padding: 20, /* The padding on each side of the picture */
-            hideflash: false, /* Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto */
-            wmode: 'opaque', /* Set the flash wmode attribute */
-            autoplay: true, /* Automatically start videos: True/False */
-            modal: false, /* If set to true, only the close button will close the window */
-            deeplinking: true, /* Allow prettyPhoto to update the url to enable deeplinking. */
-            overlay_gallery: true, /* If set to true, a gallery will overlay the fullscreen image on mouse over */
-            keyboard_shortcuts: true, /* Set to false if you open forms inside prettyPhoto */
-            changepicturecallback: function(){}, /* Called everytime an item is shown/changed */
-            callback: function(){}, /* Called when prettyPhoto is closed */
+            counter_separator_label: '/',
+            theme: 'pp_default',
+            horizontal_padding: 20,
+            hideflash: false,
+            wmode: 'opaque',
+            autoplay: true,
+            modal: false,
+            deeplinking: true,
+            overlay_gallery: true,
+            keyboard_shortcuts: true,
+            changepicturecallback: function() {},
+            callback: function() {},
             ie6_fallback: true,
-            //markup: '<div class="pp_pic_holder">...</div><div class="pp_overlay"></div>',
-            //gallery_markup: '<div class="pp_gallery">...</div>',
-            //image_markup: '...',
-            //flash_markup: '...',
-            //quicktime_markup: '...',
-            //iframe_markup: '...',
-            //inline_markup: '...',
             custom_markup: '',
-            social_tools: false /* html or false to disable */
-        });
+            social_tools: false
+        })
     }
 });
